4ea8dbd51ce1a04ee3777784e7c10c0d37a0bbf4
tangel <105234068+tangencialmente@users.noreply.github.com>
2023-10-10 16:30:45 -0300

    Add files via upload

---
+++ Bytecompile.hs
@@ -0,0 +1,158 @@
+{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE RecordWildCards #-}
+{-|
+Module      : Bytecompile
+Description : Compila a bytecode. Ejecuta bytecode.
+Copyright   : (c) Mauro Jaskelioff, Guido Martínez, 2020.
+License     : GPL-3
+Maintainer  : mauro@fceia.unr.edu.ar
+Stability   : experimental
+
+Este módulo permite compilar módulos a la Macchina. También provee
+una implementación de la Macchina para ejecutar el bytecode.
+-}
+module Bytecompile
+  (Bytecode, runBC, bcWrite, bcRead, bytecompileModule, showBC)
+ where
+
+import Lang
+import Subst
+import MonadFD4
+
+import qualified Data.ByteString.Lazy as BS
+import Data.Binary ( Word32, Binary(put, get), decode, encode )
+import Data.Binary.Put ( putWord32le )
+import Data.Binary.Get ( getWord32le, isEmpty )
+
+import Data.List (intercalate)
+import Data.Char
+
+type Opcode = Int
+type Bytecode = [Int]
+
+newtype Bytecode32 = BC { un32 :: [Word32] }
+
+{- Esta instancia explica como codificar y decodificar Bytecode de 32 bits -}
+instance Binary Bytecode32 where
+  put (BC bs) = mapM_ putWord32le bs
+  get = go
+    where go =
+           do
+            empty <- isEmpty
+            if empty
+              then return $ BC []
+              else do x <- getWord32le
+                      BC xs <- go
+                      return $ BC (x:xs)
+
+{- Estos sinónimos de patrón nos permiten escribir y hacer
+pattern-matching sobre el nombre de la operación en lugar del código
+entero, por ejemplo:
+
+   f (CALL : cs) = ...
+
+ Notar que si hubieramos escrito algo como
+   call = 5
+ no podríamos hacer pattern-matching con `call`.
+
+ En lo posible, usar estos códigos exactos para poder ejectutar un
+ mismo bytecode compilado en distintas implementaciones de la máquina.
+-}
+pattern NULL     = 0
+pattern RETURN   = 1
+pattern CONST    = 2
+pattern ACCESS   = 3
+pattern FUNCTION = 4
+pattern CALL     = 5
+pattern ADD      = 6
+pattern SUB      = 7
+pattern FIX      = 9
+pattern STOP     = 10
+pattern SHIFT    = 11
+pattern DROP     = 12
+pattern PRINT    = 13
+pattern PRINTN   = 14
+pattern JUMP     = 15
+pattern IFZ      = 16
+
+--función util para debugging: muestra el Bytecode de forma más legible.
+showOps :: Bytecode -> [String]
+showOps [] = []
+showOps (NULL:xs)        = "NULL" : showOps xs
+showOps (RETURN:xs)      = "RETURN" : showOps xs
+showOps (CONST:i:xs)     = ("CONST " ++  show i) : showOps xs
+showOps (ACCESS:i:xs)    = ("ACCESS " ++ show i) : showOps xs
+showOps (FUNCTION:i:xs)  = ("FUNCTION len=" ++ show i) : showOps xs
+showOps (CALL:xs)        = "CALL" : showOps xs
+showOps (ADD:xs)         = "ADD" : showOps xs
+showOps (SUB:xs)         = "SUB" : showOps xs
+showOps (FIX:xs)         = "FIX" : showOps xs
+showOps (STOP:xs)        = "STOP" : showOps xs
+showOps (JUMP:i:xs)      = ("JUMP off=" ++ show i) : showOps xs
+showOps (SHIFT:xs)       = "SHIFT" : showOps xs
+showOps (DROP:xs)        = "DROP" : showOps xs
+showOps (PRINT:xs)       = let (msg,_:rest) = span (/=NULL) xs
+                           in ("PRINT " ++ show (bc2string msg)) : showOps rest
+showOps (PRINTN:xs)      = "PRINTN" : showOps xs
+showOps (ADD:xs)         = "ADD" : showOps xs
+showOps (IFZ:xs)         = "IFZ" : showOps xs
+showOps (x:xs)           = show x : showOps xs
+
+showBC :: Bytecode -> String
+showBC = intercalate "; " . showOps
+
+bcc :: MonadFD4 m => TTerm -> m Bytecode
+bcc (Const i n) =  return (CONST:n:[])
+bcc (BinaryOp i Add t1 t2) = do ct1 <- bcc t1
+                                ct2 <- bcc t2
+                                return $ ct1 ++ ct2 ++ [ADD]
+bcc (BinaryOp i Sub t1 t2) = do ct1 <- bcc t1
+                                ct2 <- bcc t2
+                                return $ ct1 ++ ct2 ++ [SUB]
+bcc (BinaryOp i _ t1 t2) = failFD4 "Solo se implemento suma y resta."
+bcc (V i (Bound n)) = return $ ACCESS:n:[]
+bcc (App i t1 t2) = do ct1 <- bcc t1
+                       ct2 <- bcc t2
+                       return $ ct1 ++ ct2 ++ [CALL]
+bcc (Lam i f fty (Sc1 _ t)) = do ct <- bcc t
+                               return $ FUNCTION : len(ct) : ct 
+bcc (Print i str t) = do ct <- bcc t
+                         printFD4 str
+                         return $ [PRINTN] ++ ct ++ [NULL]
+-- falta Fix, Ifz y variables globales
+bcc (Let i x _ t1 (Sc1 _ t)) = do ct1 <- bcc t1
+                                ct  <- bcc t
+                                return $ ct1 ++ [SHIFT] ++ ct ++ [DROP]
+bcc (Fix i f _ x _ (Sc2 _ t)) = failFD4 "implementame!"
+-- Hay que modificar el if (añadir longitudes)
+bcc (IfZ _ b t1 t2) = do cb  <- bcc b
+                         ct1 <- bcc ct1
+                         ct2 <- bcc ct2
+                         return $ [IFZ] ++ cb ++ ct1 ++ ct2
+bcc f = failFD4 "implementame!"
+
+-- ord/chr devuelven los codepoints unicode, o en otras palabras
+-- la codificación UTF-32 del caracter.
+string2bc :: String -> Bytecode
+string2bc = map ord
+
+bc2string :: Bytecode -> String
+bc2string = map chr
+
+bytecompileModule :: MonadFD4 m => Module -> m Bytecode
+bytecompileModule m = failFD4 "implementame!"
+
+-- | Toma un bytecode, lo codifica y lo escribe un archivo
+bcWrite :: Bytecode -> FilePath -> IO ()
+bcWrite bs filename = BS.writeFile filename (encode $ BC $ fromIntegral <$> bs)
+
+---------------------------
+-- * Ejecución de bytecode
+---------------------------
+
+-- | Lee de un archivo y lo decodifica a bytecode
+bcRead :: FilePath -> IO Bytecode
+bcRead filename = (map fromIntegral <$> un32) . decode <$> BS.readFile filename
+
+runBC :: MonadFD4 m => Bytecode -> m ()
+runBC bc = failFD4 "implementame!"
